4a9050432a19ef12dcaa94732ae0f4f35b6f2ab7
Miek Gieben <miek@miek.nl>
2017-12-03 10:42:26 +0000

    more

--- doc.go
+++ doc.go
@@ -1,5 +1,7 @@
 /*
 Package rip implements the RIP protocol as described in RFC 2453. Both
 RIP-1 and RIP-2 Route Entries are supported.
+
+For some background on RIP and its uses see https://www.techrepublic.com/article/rip-explained-the-gory-details/ .
 */
 package rip
